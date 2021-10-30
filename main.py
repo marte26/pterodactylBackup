67a8ae42fdb1c9c3cfc708c9079536852eda79af
marte26 <me@cetinmartino.com>
2021-10-31 00:53:55 +0200

    Update main.py
    Remove download part of the script as that will be handled by the panel itself

--- main.py
+++ main.py
@@ -1,46 +1,25 @@
 import logging
 import os
 import time
-from datetime import datetime
-from pathlib import Path
-
 import requests
+
 import secrets
 
-ADMIN_KEY = secrets.ADMIN_KEY
-CLIENT_KEY = secrets.CLIENT_KEY
-BASE_URL = secrets.BASE_URL
-BASE_PATH = os.path.abspath(os.path.dirname(__file__)) + '/downloads/'
-
 CLIENT_HEADERS = {
-    "Authorization": "Bearer " + CLIENT_KEY,
+    "Authorization": "Bearer " + secrets.CLIENT_KEY,
     "Accept": "application/json",
     "Content-Type": "application/json"
 }
 
 ADMIN_HEADERS = {
-    "Authorization": "Bearer " + ADMIN_KEY,
+    "Authorization": "Bearer " + secrets.ADMIN_KEY,
     "Accept": "application/json",
     "Content-Type": "application/json"
 }
 
 
-def downloadfromurl(url, path, filename):
-    Path(path).mkdir(parents=True, exist_ok=True)
-
-    if not path.endswith("/"):
-        path += "/"
-
-    r = requests.get(url, stream=True)
-
-    with open(path + filename, "wb") as file:
-        for chunk in r.iter_content(chunk_size=52428800):
-            if chunk:
-                file.write(chunk)
-
-
 def get_backups(serverid):
-    url = BASE_URL + serverid + '/backups'
+    url = secrets.CLIENT_URL + serverid + '/backups'
 
     response = requests.get(url, headers=CLIENT_HEADERS).json()
 
@@ -48,13 +27,13 @@ def get_backups(serverid):
 
 
 def delete_backup(serverid, backupuuid):
-    url = BASE_URL + serverid + '/backups/' + backupuuid
+    url = secrets.CLIENT_URL + serverid + '/backups/' + backupuuid
 
     requests.delete(url, headers=CLIENT_HEADERS)
 
 
 def create_backup(serverid):
-    url = BASE_URL + serverid + '/backups'
+    url = secrets.CLIENT_URL + serverid + '/backups'
 
     response = requests.post(url=url, headers=CLIENT_HEADERS).json()
 
@@ -62,22 +41,11 @@ def create_backup(serverid):
 
 
 def isbackupfinished(serverid, backupuuid):
-    url = BASE_URL + serverid + '/backups/' + backupuuid
+    url = secrets.CLIENT_URL + serverid + '/backups/' + backupuuid
 
     response = requests.get(url, headers=CLIENT_HEADERS).json()
 
     return response['attributes']['completed_at']
-
-
-def downlod_backup(backupuuid, serverid, servername):
-    url = BASE_URL + serverid + '/backups/' + backupuuid + '/download'
-    downloadurl = requests.get(url, headers=CLIENT_HEADERS).json()['attributes']['url']
-    downloadpath = BASE_PATH + servername + '/'
-    filename = servername + '-' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.tar.gz'
-
-    logging.info("Starting download of " + servername)
-    downloadfromurl(downloadurl, downloadpath, filename)
-    logging.info("Download completed")
 
 
 def make_backups(servers):
@@ -97,13 +65,9 @@ def make_backups(servers):
             time.sleep(5)
         logging.info("Backup finished for " + i['name'])
 
-        downlod_backup(backupuuid, i['identifier'], i['name'])
-
 
 def get_servers():
-    url = 'https://badsl.nl/api/application/servers'
-
-    response = requests.get(url, headers=ADMIN_HEADERS).json()
+    response = requests.get(secrets.ADMIN_URL, headers=ADMIN_HEADERS).json()
 
     serverlistraw = response['data']
     servercount = response['meta']['pagination']['total']
