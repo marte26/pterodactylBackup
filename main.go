391ed7a992e23355c90d281b92a6de3e76b77b8e
marte26 <me@cetinmartino.com>
2022-12-04 01:13:51 +0100

    first working prototype

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/marte26/pterodactylBackup/pterodactylapi/pterodactyladminapi"
-	"github.com/marte26/pterodactylBackup/pterodactylapi/pterodactylclientapi"
+	"github.com/marte26/pterodactylBackup/pterodactylapi/adminapi"
+	"github.com/marte26/pterodactylBackup/pterodactylapi/clientapi"
+	"github.com/marte26/pterodactylBackup/pterodactylapi/structs"
 
 	"github.com/spf13/viper"
 )
@@ -39,26 +40,56 @@ func main() {
 		log.Fatal("cannot load config: ", err)
 	}
 
-	adminAPI := pterodactyladminapi.Client{
+	adminAPI := adminapi.Client{
 		URL:    config.BaseURL,
 		APIKey: config.APIKey,
 	}
-	clientAPI := pterodactylclientapi.Client{
+	clientAPI := clientapi.Client{
 		URL:    config.BaseURL,
 		APIKey: config.APIKey,
 	}
 
 	servers, err := adminAPI.GetServers()
 	if err != nil {
-		log.Fatal("cannot get servers:", err)
+		log.Fatalf("cannot get servers: %v", err)
 	}
 
-	files, err := clientAPI.GetFiles(servers[0].Attributes.Identifier, "/world")
-
-	printJson(files)
+	for _, server := range servers {
+		createBackup(clientAPI, server)
+	}
 }
 
-func printJson(s any) {
+func createBackup(clientAPI clientapi.Client, server structs.Server) {
+	if server.Attributes.FeatureLimits.Backups == 0 {
+		log.Printf("backups for server %v not allowed, skipping", server.Attributes.Name)
+		return
+	}
+
+	backups, err := clientAPI.GetBackups(server.Attributes.Identifier)
+	if err != nil {
+		log.Printf("cannot get backups for server %v: %v", server.Attributes.Name, err)
+		return
+	}
+
+	if len(backups) >= server.Attributes.FeatureLimits.Backups {
+		log.Printf("backup limit of %v reached, deleting oldest backup", server.Attributes.FeatureLimits.Backups)
+		err := clientAPI.DeleteBackup(server.Attributes.Identifier, backups[0].Attributes.UUID)
+		if err != nil {
+			log.Printf("cannot delete backup %v for server %v: %v", backups[0].Attributes.UUID, server.Attributes.Name, err)
+			return
+		}
+	}
+
+	response, err := clientAPI.CreateBackup(server.Attributes.Identifier)
+	if err != nil {
+		log.Printf("cannot create backup for server %v: %v", server.Attributes.Name, err)
+		return
+	}
+
+	log.Printf("creating backup %v for server %v", response.Attributes.UUID, server.Attributes.Name)
+}
+
+func printJSON(s any) {
 	jsonIndent, _ := json.MarshalIndent(s, "", "    ")
 
 	fmt.Println(string(jsonIndent))
